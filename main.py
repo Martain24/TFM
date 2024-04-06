d7411eb41b3225964c61dbe3c833a432ccb66bc9
Martain24 <vilaboamartin@gmail.com>
2024-04-06 14:06:25 +0200

    movidas

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+st.write(f"""
+# TFM
+Bienvenidos a nuestro pedazo de TFM
+Aquí tenéis nuestro dataframe      
+""")
+
+df
