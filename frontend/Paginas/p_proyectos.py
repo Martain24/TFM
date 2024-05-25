f02402856780d449b4c8f3570d7465ed0458c457
miguelrd99 <miguelrode@gmail.com>
2024-05-25 21:11:24 +0200

    creación project_prediccion

--- frontend/Paginas/p_proyectos.py
+++ frontend/Paginas/p_proyectos.py
@@ -1,17 +1,20 @@
 import streamlit as st 
 from .proyectos import project_logistic_test
 from .proyectos import project_prediccion_consumo
+from .proyectos import project_prediccion
 
 
 
 def proyectos():
-    titulos_proyectos = ["Logistic Test", "Predicción de consumo"]
+    titulos_proyectos = ["Logistic Test", "Predicción de consumo", "Predicción"]
     opcion = st.selectbox("Selecciona un proyecto", titulos_proyectos)
 
     if opcion == "Logistic Test":
         project_logistic_test.logistic_test()
     elif opcion == "Predicción de consumo":
         project_prediccion_consumo.prediccion_consumo()
+    elif opcion == "Predicción":
+        project_prediccion.prediccion()
 
 
 
